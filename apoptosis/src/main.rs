1a1af7f39b2b4afdca068c39a8aa3354513ecd46
perillamint <perillamint@silicon.moe>
2023-12-08 22:30:12 +0900

    Initial command parameter implementation

--- apoptosis/src/main.rs
+++ apoptosis/src/main.rs
@@ -1,8 +1,45 @@
-
+use crate::types::InstanceType;
+use clap::{Parser, Subcommand};
 
 mod caspase;
 mod trail;
+mod types;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Subcommand, Clone, Debug)]
+enum Command {
+    Convert {
+        #[arg(long, help = "Instance type to run Apoptosis on")]
+        instance_type: InstanceType,
+
+        #[arg(long, help = "Database URL to run Apoptosis on")]
+        database_url: String,
+    },
+    Destruct {
+        #[arg(long, help = "Listen address")]
+        listen: String,
+
+        #[arg(long, help = "Request parallel per instance")]
+        connection_per_instance: i32,
+
+        #[arg(long, help = "Task runner concurrency")]
+        thread_cnt: i32,
+    },
+}
+
+#[derive(Parser, Clone, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    tracing::info!("Apotosis initialized.");
+
+    //
+
+    Ok(())
 }
