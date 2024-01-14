da1e0fa34ebb9c763c0a7e5456d1e84d023c9455
perillamint <perillamint@silicon.moe>
2024-01-15 00:18:52 +0900

    Stash

---
+++ apoptosis/src/trail/datafetcher/misskey13.rs
@@ -0,0 +1,14 @@
+
+const USER_FETCH_QUERY: &str = """
+SELECT
+  "user".id AS id,
+  "user".username AS username,
+  "user_keypair"."privateKey" AS private_key
+FROM "user"
+INNER JOIN "user_keypair" ON "user_keypair"."userId" = "user".id
+WHERE "user"."isDeleted" IS FALSE
+ORDER BY "user".id
+LIMIT ? OFFSET ?;
+"""
+
+//
--- apoptosis/src/trail/datafetcher/mod.rs
+++ apoptosis/src/trail/datafetcher/mod.rs
@@ -1,2 +1,16 @@
 mod mastodon;
 mod misskey13;
+
+pub struct User {
+    /// User's unique identifier. Misskey will use bizarre aid/aidx
+    /// and Mastodon will use timeflake for it
+    id: String,
+    /// Username, of course.
+    username: String,
+    /// PEM encoded RSA-2048 private key block. As most Fediverse instance uses
+    private_key: String,
+}
+
+impl User {
+    //
+}
